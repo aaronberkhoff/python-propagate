9c590155bb5b8fa97d48db3f435dc8f9b1728be8
Aaron Berkhoff <aaronberkhoff@outlook.com>
2025-05-21 13:14:52 -0500

    fixed bug with headers part 2

--- src/python_propagate/connectors/astria.py
+++ src/python_propagate/connectors/astria.py
@@ -212,7 +212,7 @@ class AstriaConnector:
                     'VZ_BODY_FIXED_KMS': state.velocity_ecef[2],
 
                     'LAT_DEG': state.latlong[0] *  RAD2DEG,
-                    'LAT_DEG': state.latlong[1] * RAD2DEG,
+                    'LON_DEG': state.latlong[1] * RAD2DEG,
                     'ALT_KM': np.linalg.norm(state.position_eci) - 6378.1363 # Radius of Earth
                 })
 
@@ -307,7 +307,7 @@ class AstriaConnector:
                         'VZ_BODY_FIXED_KMS': state.velocity_ecef[2],
 
                         'LAT_DEG': state.latlong[0] *  RAD2DEG,
-                        'LAT_DEG': state.latlong[1] * RAD2DEG,
+                        'LON_DEG': state.latlong[1] * RAD2DEG,
                         'ALT_KM': np.linalg.norm(state.position_eci) - 6378.1363 # Radius of Earth
                     })
                 self.propagator.state_data = []
