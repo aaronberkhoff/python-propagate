1fb0c1271896f338df8359c2a16e84a82d08cd4f
aaronberkhoff <aaronberkhoff@outlook.com>
2025-06-09 12:59:12 -0600

    Added new xgeo case in the final_cases yaml file. Changes spice kernel

--- src/python_propagate/utilities/load_spice.py
+++ src/python_propagate/utilities/load_spice.py
@@ -24,12 +24,14 @@ def load_spice():
     """Load the necessary SPICE kernels for the simulation."""
     spice.furnsh(str(DATA_DIR / "naif0011.tls"))
     spice.furnsh(str(DATA_DIR / "earth_000101_250421_250124.bpc"))
-    spice.furnsh(str(DATA_DIR / "de430.bsp"))
+    # spice.furnsh(str(DATA_DIR / "de430.bsp"))
+    spice.furnsh(str(DATA_DIR / "de442.bsp"))
     # spice.furnsh(DATA_DIR / "pck00010.tpc")
 
 def unload_spice():
     """Unload the SPICE kernels from the simulation."""
     spice.unload((DATA_DIR / "naif0011.tls"))
     spice.unload((DATA_DIR / "earth_000101_250421_250124.bpc"))
-    spice.unload((DATA_DIR / "de430.bsp"))
+    # spice.unload((DATA_DIR / "de430.bsp"))
+    spice.unload(str(DATA_DIR / "de442.bsp"))
     # spice.unload(DATA_DIR / "pck00010.tpc")
